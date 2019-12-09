16faef4073e9184727dfd8ad8cdb27ff0f265cea
Balthild <ibalthild@gmail.com>
2019-12-09 18:27:01 +0800

    Init

---
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using ReactiveUI;
+
+namespace AvaloniaApplication1.ViewModels {
+    public class MainWindowViewModel : ViewModelBase {
+        private string[] leakedItems = { };
+
+        public string[] LeakedItems {
+            get => leakedItems;
+            set => this.RaiseAndSetIfChanged(ref leakedItems, value);
+        }
+
+        private bool aLot = true;
+
+        public void RefreshList() {
+            int count = aLot ? 50 : 1;
+            aLot = !aLot;
+
+            LeakedItems = Enumerable.Range(0, count)
+                .Select(_ => "Test Test Test")
+                .ToArray();
+        }
+    }
+}
